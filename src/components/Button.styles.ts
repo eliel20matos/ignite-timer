3f4bca2ea36ff778bcc07dbb2b7f2e3108d08d6d
eliel20matos <eliellimamatos@gmail.com>
2022-09-08 21:29:48 -0400

    feat: defining default definition tyés

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,11 +17,13 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
 
+    background-color: ${props => props.theme.primary};
+
     //sempre que uso essa sintaxe, o styled
     // components vai enviar todas as props, como se fosse uma função
-    ${props => {
+    /* ${props => {
         return css`
             background-color: ${buttonVariants[props.variant]}
         `
-    }}
+    }} */
 `
